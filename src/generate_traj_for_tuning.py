3df252769d51e6f1ea57902add949766b69ebc31
zehuilu <zehuilu@umich.edu>
2020-10-05 11:25:10 -0400

    update generate_traj_for_tuning.csv

--- src/generate_traj_for_tuning.py
+++ src/generate_traj_for_tuning.py
@@ -3,6 +3,7 @@ import os
 import sys
 sys.path.append(os.getcwd()+'/lib')
 import numpy as np
+import json
 from SplineByPeakSpeed import SplineByPeakSpeed
 
 
@@ -13,14 +14,18 @@ if __name__ == "__main__":
     v_peak = np.array([[0.5], [0.5], [0.5]])
     t_peak = 1.5
     t_total = 3.0
-    dt = 0.1
+    dt = 0.5
 
     # initialize the class
     spline_class = SplineByPeakSpeed(p_0, v_0, a_0, v_peak, t_peak, t_total, dt)
     # generate the spline trajectory
     T, traj = spline_class.do_calculation()
 
-    print("This is T.")
-    print(T)
-    print("This is traj.")
-    print(traj)
+    array_csv = np.vstack((T, traj[0:6, :]))
+    print("This is csv.")
+    print(array_csv)
+
+    json_file = open("config.json")
+    config_data = json.load(json_file)
+    filename_csv = os.getcwd() + config_data["DIRECTORY_TRAJ"] + "traj.csv"
+    np.savetxt(filename_csv, array_csv, delimiter=",")
